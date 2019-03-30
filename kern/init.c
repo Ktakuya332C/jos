a7c6ca81c3dae0a0250172ca58420a1fc3f6eeda
Takuya Kato <ktakuya@Takuyas-MacBook-Pro.local>
2019-03-30 20:47:32 +0900

    Add a global variable in init.c

--- kern/init.c
+++ kern/init.c
@@ -1,7 +1,9 @@
 #include <inc/string.h>
 
+int mem_init = 0;
+
 void i386_init(void) {
   extern char edata[], end[];
-  memset(edata, 0, end - edata);
+  memset(edata, mem_init, end - edata);
   while(1);
 }
