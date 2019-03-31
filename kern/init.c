25274651e950606f693a3b3b9eb41f4b5a7f7d67
Takuya Kato <ktakuya@Takuyas-MacBook-Pro.local>
2019-03-31 16:32:11 +0900

    Add console.c

--- kern/init.c
+++ kern/init.c
@@ -1,7 +1,18 @@
 #include <inc/string.h>
+#include <kern/console.h>
 
 void i386_init(void) {
   extern char edata[], end[];
+  
+  // Clear uninitialized global data section (bss)
   memset(edata, 0, end - edata);
+  
+  // Initialize the console
+  cons_init();
+  
+  // Put characters A and B
+  cons_putc(0x41);
+  cons_putc(0x42);
+  
   while(1);
 }
