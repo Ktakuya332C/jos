fa56611feb6dc2b30c9ae19dc2d8983ab5f4139b
Takuya Kato <ktakuya@Takuyas-MacBook-Pro.local>
2019-04-06 15:22:30 +0900

    Add a monitor command help

--- inc/string.h
+++ inc/string.h
@@ -5,6 +5,8 @@
 
 int strlen(const char* s);
 int strnlen(const char* s, size_t size);
+int strcmp(const char *s1, const char *s2);
+char* strchr(const char *s, char c);
 
 void* memset(void* dst, int c, size_t len);
 void* memmove(void* dst, const void* src, size_t n);
