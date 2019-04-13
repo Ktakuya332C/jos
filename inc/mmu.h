bc1d775425532daf034969f79e469da8ea66276d
Takuya Kato <ktakuya@Takuyas-MacBook-Pro.local>
2019-04-13 17:24:16 +0900

    Remove duplicate definition

--- inc/mmu.h
+++ inc/mmu.h
@@ -53,15 +53,4 @@
 #define CR0_CD 0x40000000	// Cache Disable
 #define CR0_PG 0x80000000	// Paging
 
-// Page directory/table entry flags
-#define PTE_P 0x001	// Present
-#define PTE_W 0x002	// Writeable
-#define PTE_U 0x004	// User
-#define PTE_PWT 0x008	// Write-Through
-#define PTE_PCD 0x010	// Cache-Disable
-#define PTE_A 0x020	// Accessed
-#define PTE_D 0x040	// Dirty
-#define PTE_PS 0x080	// Page Size
-#define PTE_G 0x100	// Global
-
 #endif // INC_MMU_H
